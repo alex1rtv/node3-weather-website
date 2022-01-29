622dbf0b47fe9b7908ff5f3dd0f9eb8b2d6a6f7e
alex1rtv <alex1rtv@gmail.com>
2022-01-29 14:37:19 +0200

    set up app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -29,7 +29,7 @@ weatherForm.addEventListener('submit', (event) => {
     const location = search.value;
     //console.log(location);
 
-    fetch(`http://localhost:3000/weather?address=${location}`)
+    fetch(`/weather?address=${location}`)
     .then((response) => { 
         response.json()
         .then((data) => {
--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const geocode = require('./utils/geocode');
 const weather = require('./utils/weather');
  
 const app = express();
+const port = process.env.PORT || 3000;
 
 // console.log(__dirname);
 // console.log(__filename);
@@ -133,4 +134,4 @@ app.get('*', (request, response) => {
         name: 'Bob'});
 });
 
-app.listen(3000, () => console.log('Server is up on port 3000'));
+app.listen(port, () => console.log(`Server is up on port ${port}`));
